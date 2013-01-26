10c339d40de7b8fc6fcb02feb822d68831ecf469
Blake Robertson <blaquer@gmail.com>
2013-01-26 18:51:19 -0500

    Merge

---
+++ Platformer/Platformer/Screens/GameOverScreen.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Unicorn.ScreenArchitecture;
+using Microsoft.Xna.Framework;
+
+namespace Platformer.Screens
+{
+    public enum Ending
+    {
+        Win,
+        Diabetes,
+        TimeOut,
+        Death
+    }
+    public class GameOverScreen : GameScreen
+    {
+        public Ending Ending { get; set; }
+
+        public GameOverScreen(Ending ending)
+        {
+            Ending = ending;
+        }
+
+        public void HandleInput()
+        {
+
+        }
+
+        public void Draw()
+        {
+            ScreenManager.GraphicsDevice.Clear(Color.Black);
+
+            ScreenManager.SpriteBatch.Begin();
+            switch (Ending)
+            {
+                case Ending.Win:
+                    ScreenManager.SpriteBatch.DrawString(ScreenManager.Font, "Win!", new Vector2(1280 / 2, 720 / 2), Color.White);
+                    break;
+                case Ending.Diabetes:
+                    ScreenManager.SpriteBatch.DrawString(ScreenManager.Font, "Diabetes!", new Vector2(1280 / 2, 720 / 2), Color.White);
+                    break;
+                case Ending.TimeOut:
+                    ScreenManager.SpriteBatch.DrawString(ScreenManager.Font, "Time Out!", new Vector2(1280 / 2, 720 / 2), Color.White);
+                    break;
+                case Ending.Death:
+                    ScreenManager.SpriteBatch.DrawString(ScreenManager.Font, "Death!", new Vector2(1280 / 2, 720 / 2), Color.White);
+                    break;
+                default:
+                    break;
+            }
+
+            ScreenManager.SpriteBatch.End();
+        }
+    }
+}
