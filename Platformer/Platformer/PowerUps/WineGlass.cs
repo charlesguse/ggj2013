9d03c044d53a4be037a399ace58863f4197a9a8d
heydottim <hey.tim@gmail.com>
2013-01-26 10:09:26 -0500

    Adding the unicorn, finally.

---
+++ Platformer/Platformer/PowerUps/WineGlass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Unicorn.PowerUps;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework;
+
+namespace Unicorn.PowerUps
+{
+    public class WineGlass : Gem
+    {
+        public WineGlass(Level level, Vector2 position) :
+            base(level, position)
+        {
+
+        }
+
+        public override void LoadContent()
+        {
+            Texture = Level.Content.Load<Texture2D>("Sprites/PowerUps/Goblet filled stem");
+            //origin = new Vector2(texture.Width / 2.0f, texture.Height / 2.0f);
+            CollectedSound = Level.Content.Load<SoundEffect>("Sounds/GemCollected");
+            base.LoadContent();
+        }
+
+        public override void OnCollected(Player collectedBy)
+        {
+            Level.TimeRemaining += new TimeSpan(0, 0, 15);
+            base.OnCollected(collectedBy);
+        }
+    }
+}
