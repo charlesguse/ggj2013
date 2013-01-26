56a517e8dd7e6affbace6bbac7a939d5d73ee36b
Charlie Guse <charlesguse@users.noreply.github.com>
2013-01-26 13:05:50 -0500

    Added the journal pieces to the game

---
+++ Platformer/Platformer/Screens/MainMenuScreen.cs
@@ -0,0 +1,141 @@
+#region File Description
+//-----------------------------------------------------------------------------
+// MainMenuScreen.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using Microsoft.Xna.Framework;
+using Unicorn.Screens;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using Microsoft.Xna.Framework.Graphics;
+#endregion
+
+namespace Unicorn
+{
+    /// <summary>
+    /// The main menu screen is the first thing displayed when the game starts up.
+    /// </summary>
+    class MainMenuScreen : MenuScreen
+    {
+        #region Initialization
+        List<string> lines;
+        public Vector2 DreamTextPosition { get; set; }
+        public SpriteFont DreamFont { get; set; }
+
+        /// <summary>
+        /// Constructor fills in the menu contents.
+        /// </summary>
+        public MainMenuScreen()
+            : base("")
+        {
+            // Create our menu entries.
+            MenuEntry playGameMenuEntry = new MenuEntry("Play Game");
+            MenuEntry optionsMenuEntry = new MenuEntry("Options");
+            MenuEntry exitMenuEntry = new MenuEntry("Exit");
+
+            // Hook up menu event handlers.
+            playGameMenuEntry.Selected += PlayGameMenuEntrySelected;
+            optionsMenuEntry.Selected += OptionsMenuEntrySelected;
+            exitMenuEntry.Selected += OnCancel;
+
+            // Add entries to the menu.
+            MenuEntries.Add(playGameMenuEntry);
+            MenuEntries.Add(optionsMenuEntry);
+            MenuEntries.Add(exitMenuEntry);
+        }
+
+        public override void LoadContent()
+        {
+            DreamTextPosition = new Vector2(700, 100);
+            DreamFont = ScreenManager.Content.Load<SpriteFont>("Fonts/dreamfont");
+            int width;
+            lines = new List<string>();
+            using (StreamReader reader = new StreamReader("Content/Story/intro.txt"))
+            {
+                string line = reader.ReadLine();
+                width = line.Length;
+                while (line != null)
+                {
+                    lines.Add(line);
+                    //if (line.Length != width)
+                    //    throw new Exception(String.Format("The length of line {0} is different from all preceeding lines.", lines.Count));
+                    line = reader.ReadLine();
+                }
+            }
+            base.LoadContent();
+        }
+        #endregion
+
+        #region Handle Input
+
+
+        /// <summary>
+        /// Event handler for when the Play Game menu entry is selected.
+        /// </summary>
+        void PlayGameMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        {
+            LoadingScreen.Load(ScreenManager, true, e.PlayerIndex,
+                               new GameplayScreen());
+        }
+
+
+        /// <summary>
+        /// Event handler for when the Options menu entry is selected.
+        /// </summary>
+        void OptionsMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        {
+            ScreenManager.AddScreen(new OptionsMenuScreen(), e.PlayerIndex);
+        }
+
+
+        /// <summary>
+        /// When the user cancels the main menu, ask if they want to exit the sample.
+        /// </summary>
+        protected override void OnCancel(PlayerIndex playerIndex)
+        {
+            const string message = "Are you sure you want to exit this sample?";
+
+            MessageBoxScreen confirmExitMessageBox = new MessageBoxScreen(message);
+
+            confirmExitMessageBox.Accepted += ConfirmExitMessageBoxAccepted;
+
+            ScreenManager.AddScreen(confirmExitMessageBox, playerIndex);
+        }
+
+
+        /// <summary>
+        /// Event handler for when the user selects ok on the "are you sure
+        /// you want to exit" message box.
+        /// </summary>
+        void ConfirmExitMessageBoxAccepted(object sender, PlayerIndexEventArgs e)
+        {
+            ScreenManager.Game.Exit();
+        }
+
+
+        #endregion
+
+        public override void Draw(GameTime gameTime)
+        {
+            Vector2 offset = Vector2.Zero;
+            base.Draw(gameTime);
+
+            ScreenManager.SpriteBatch.Begin();
+            
+            foreach (var line in lines)
+            {
+                ScreenManager.SpriteBatch.DrawString(DreamFont, line, DreamTextPosition + offset, Color.Wheat);
+                offset.Y += 20;
+            }
+
+            ScreenManager.SpriteBatch.End();
+        }
+
+    }
+}
