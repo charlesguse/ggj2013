3605fbfa8460a53ff15fe5fab4f37d76fd1ad139
Blake Robertson <blaquer@gmail.com>
2013-01-26 18:20:46 -0500

    Added ability for passable tiles and items to be in same location; also added slug sprite

---
+++ Platformer/Platformer/PowerUps/Salmon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Unicorn.PowerUps
+{
+    public class Salmon : Gem
+    {
+        public Salmon(Level level, Vector2 position) :
+            base(level, position)
+        {
+
+        }
+
+        public override void LoadContent()
+        {
+            Texture = Level.ScreenManager.Content.Load<Texture2D>("Sprites/Powerups/Salmon");
+            //origin = new Vector2(texture.Width / 2.0f, texture.Height / 2.0f);
+            CollectedSound = Level.ScreenManager.Content.Load<SoundEffect>("Sounds/FISH SFX");
+            base.LoadContent();
+        }
+
+        public override void OnCollected(Player collectedBy)
+        {
+            Level.TimeRemaining += new TimeSpan(0, 0, 15);
+            Level.Player.Fattyfatness += 0.02f;
+            base.OnCollected(collectedBy);
+        }
+    }
+}
---
+++ Platformer/Platformer/PowerUps/WineGlass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Unicorn.PowerUps;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework;
+
+namespace Unicorn.PowerUps
+{
+    public class WineGlass : Gem
+    {
+        public WineGlass(Level level, Vector2 position) :
+            base(level, position)
+        {
+
+        }
+
+        public override void LoadContent()
+        {
+            Texture = Level.ScreenManager.Content.Load<Texture2D>("Sprites/PowerUps/Goblet filled stem");
+            //origin = new Vector2(texture.Width / 2.0f, texture.Height / 2.0f);
+            CollectedSound = Level.ScreenManager.Content.Load<SoundEffect>("Sounds/WINE SFX");
+            base.LoadContent();
+        }
+
+        public override void OnCollected(Player collectedBy)
+        {
+            Level.TimeRemaining += new TimeSpan(0, 0, 15);
+            Level.Player.Fattyfatness -= 0.02f;
+            base.OnCollected(collectedBy);
+        }
+    }
+}
