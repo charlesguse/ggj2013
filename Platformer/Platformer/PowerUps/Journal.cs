e0e50b5e6b77bfebb678674064224459e63ce834
Blake Robertson <blaquer@gmail.com>
2013-01-26 23:51:00 -0500

    Sorta fixed the unicorn's collision

---
+++ Platformer/Platformer/PowerUps/Journal.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Unicorn.PowerUps;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework;
+using System.IO;
+
+namespace Unicorn.PowerUps
+{
+    public class Journal : Gem
+    {
+        public static int journalPiece = 0;
+        public static int journalMax = 0;
+        public static List<string> JournalDoc = null;
+
+        public Journal(Level level, Vector2 position) :
+            base(level, position)
+        {
+
+        }
+
+        public override void LoadContent()
+        {
+            if (JournalDoc == null)
+            {
+                LoadStories();
+            }
+
+            //Texture = Level.Content.Load<Texture2D>("Sprites/PowerUps/Goblet filled stem");
+            Texture = Level.ScreenManager.Content.Load<Texture2D>("Sprites/PowerUps/Journal");
+            //origin = new Vector2(texture.Width / 2.0f, texture.Height / 2.0f);
+            CollectedSound = Level.ScreenManager.Content.Load<SoundEffect>("Sounds/PAPER SFX");
+            base.LoadContent();
+        }
+
+        public static void LoadStories()
+        {
+            int i = 0;
+
+            try
+            {
+                JournalDoc = new List<string>();
+                while (true)
+                {
+                    using (StreamReader reader = new StreamReader(string.Format("Content/Story/{0}.txt", i++)))
+                    {
+                        string line = reader.ReadToEnd();
+                        if (line != null)
+                        {
+                            JournalDoc.Add(line);
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                journalMax = i - 1;
+            }
+        }
+
+        public override void OnCollected(Player collectedBy)
+        {
+            MessageBoxScreen confirmExitMessageBox;
+            //Level.TimeRemaining += new TimeSpan(0, 0, 15);
+            if (journalPiece < journalMax)
+            {
+                confirmExitMessageBox = new MessageBoxScreen(JournalDoc[journalPiece++]);
+            }
+            else
+            {
+                confirmExitMessageBox = new MessageBoxScreen("You have found all of the journal entries");
+            }
+            Level.ScreenManager.AddScreen(confirmExitMessageBox, null);
+
+            base.OnCollected(collectedBy);
+        }
+
+        public static bool AllJournalPiecesFound()
+        {
+            if (JournalDoc == null)
+            {
+                LoadStories();
+            }
+            return journalPiece >= journalMax;
+        }
+    }
+}
---
+++ Platformer/Platformer/Screens/GameOverScreen.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Unicorn.ScreenArchitecture;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Unicorn.Screens;
+using Unicorn;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Platformer.Screens
+{
+    public enum Ending
+    {
+        Win,
+        Diabetes,
+        TimeOut,
+        Death
+    }
+    public class GameOverScreen : GameScreen
+    {
+        public Ending Ending { get; set; }
+        private Texture2D background { get; set; }
+        private Texture2D wilford { get; set; }
+        private Texture2D diabeetus { get; set; }
+
+        public GameOverScreen(Ending ending)
+        {
+            Ending = ending;
+        }
+
+        public override void LoadContent()
+        {
+            background = ScreenManager.Content.Load<Texture2D>("Backgrounds/endGame");
+            //wilford = ScreenManager.Content.Load<Texture2D>("Backgrounds/wilfordHead");
+            //diabeetus = ScreenManager.Content.Load<Texture2D>("Backgrounds/diabeetus");
+
+            base.LoadContent();
+        }
+
+        public override void HandleInput(InputState input)
+        {
+            if (input == null)
+                throw new ArgumentNullException("input");
+
+            if (input.IsPauseGame(ControllingPlayer))
+            {
+                LoadingScreen.Load(ScreenManager, true, null,
+                    new BackgroundScreen(),
+                    new MainMenuScreen());
+            }
+            //LoadingScreen.Load(Level.ScreenManager, true, null,
+            //                   new GameOverScreen(Ending.Death));
+        }
+
+        public override void  Draw(GameTime gameTime)
+        {
+            ScreenManager.GraphicsDevice.Clear(Color.Black);
+
+            ScreenManager.SpriteBatch.Begin();
+            //Scree
+
+            switch (Ending)
+            {
+                case Ending.Win:
+                    ScreenManager.SpriteBatch.DrawString(ScreenManager.Font, "Win!", new Vector2(1280 / 2, 720 / 2), Color.White);
+                    break;
+                case Ending.Diabetes:
+                    ScreenManager.SpriteBatch.DrawString(ScreenManager.Font, "Diabetes!", new Vector2(1280 / 2, 720 / 2), Color.White);
+                    break;
+                case Ending.TimeOut:
+                    ScreenManager.SpriteBatch.DrawString(ScreenManager.Font, "Time Out!", new Vector2(1280 / 2, 720 / 2), Color.White);
+                    break;
+                case Ending.Death:
+                    ScreenManager.SpriteBatch.DrawString(ScreenManager.Font, "Death!", new Vector2(1280 / 2, 720 / 2), Color.White);
+                    break;
+                default:
+                    break;
+            }
+            ScreenManager.SpriteBatch.End();
+        }
+    }
+}
